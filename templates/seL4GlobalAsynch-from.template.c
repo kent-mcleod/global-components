6f592ecb20b2477af89a1839d2611c19f3ab7969
Kent McLeod <kent@kry10.com>
2023-01-20 09:45:18 +1100

    multicore

--- templates/seL4GlobalAsynch-from.template.c
+++ templates/seL4GlobalAsynch-from.template.c
@@ -9,11 +9,33 @@
 
 /*? macros.show_includes(me.instance.type.includes) ?*/
 
+/*- set nodes = set() -*/
+/*- for end in me.parent.from_ends -*/
+    /*- do nodes.add(render_state.label_node_map[end.instance.name]) -*/
+/*- endfor -*/
+
+/*- for end in me.parent.to_ends -*/
+    /*- do nodes.add(render_state.label_node_map[end.instance.name]) -*/
+/*- endfor -*/
+
+/*- set multicore = len(nodes) > 1 -*/
+
+
+
 /*- for c in me.parent.to_ends -*/
-    /*- do allocate_cap(c, is_reader=False) -*/
+    /*- do allocate_cap(c, is_reader=False, multicore=multicore) -*/
+    /*- if multicore -*/
+    void /*? me.interface.name ?*/_emit_underlying(void) {
+        /*- set (notification, sgi, node_id) = pop('notification') -*/
+
+        seL4_ARM_SGI_Signal_Generate(/*? notification ?*/, /*? sgi ?*/, 1lu <<(/*? node_id ?*/));
+    }
+    /*- else -*/
+
     /*- set notification = pop('notification') -*/
 
     void /*? me.interface.name ?*/_emit_underlying(void) {
         seL4_Signal(/*? notification ?*/);
     }
+    /*- endif -*/
 /*- endfor -*/
